57c823a3093d64c271b218dc4ebe6f44c33b8947
Oliver Grande <oliver.grande@sap.com>
2017-03-03 14:53:49 +0100

    SetAttributesDeep for ModifyUtil

--- jpa/odata-jpa-processor/src/main/java/com/sap/olingo/jpa/processor/core/processor/JPAModifyUtil.java
+++ jpa/odata-jpa-processor/src/main/java/com/sap/olingo/jpa/processor/core/processor/JPAModifyUtil.java
@@ -1,5 +1,6 @@
 package com.sap.olingo.jpa.processor.core.processor;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Map;
@@ -19,7 +20,7 @@ public final class JPAModifyUtil {
         Object value = jpaAttributes.get(attributeName);
 
         if (value != null) {
-          if (!(value instanceof Map<?, ?>)) {
+          if (!(value instanceof Map<?, ?>) && !(value instanceof JPARequestEntity)) {
             try {
               Class<?>[] parameters = meth.getParameterTypes();
               if (parameters.length == 1 && value.getClass() == parameters[0]) {
@@ -30,6 +31,65 @@ public final class JPAModifyUtil {
             } catch (IllegalArgumentException e) {
               throw new ODataJPAProcessorException(e, HttpStatusCode.INTERNAL_SERVER_ERROR);
             } catch (InvocationTargetException e) {
+              throw new ODataJPAProcessorException(e, HttpStatusCode.INTERNAL_SERVER_ERROR);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   * Fills instanze and its embedded components. In case of embedded components first it is tried to get an existing
+   * instance. If non is provided a new one is created and set.
+   * @param jpaAttributes
+   * @param instanze
+   * @throws ODataJPAProcessorException
+   */
+  @SuppressWarnings("unchecked")
+  public void setAttributesDeep(final Map<String, Object> jpaAttributes, final Object instanze)
+      throws ODataJPAProcessorException {
+    final Method[] methods = instanze.getClass().getMethods();
+    for (final Method meth : methods) {
+      if (meth.getName().substring(0, 3).equals("set")) {
+        final String attributeName = meth.getName().substring(3, 4).toLowerCase() + meth.getName().substring(4);
+        final Object value = jpaAttributes.get(attributeName);
+
+        if (value != null) {
+          Class<?>[] parameters = meth.getParameterTypes();
+          if (!(value instanceof JPARequestEntity) && parameters.length == 1) {
+            try {
+              if (!(value instanceof Map<?, ?>)) {
+                if (value.getClass() == parameters[0]) {
+                  meth.invoke(instanze, value);
+                }
+              } else {
+                final String getterName = "g" + meth.getName().substring(1);
+                final Class<?>[] parameter = new Class<?>[0];
+                final Method getter = instanze.getClass().getMethod(getterName, parameter);
+                Object embedded = null;
+                if (getter != null)
+                  embedded = instanze.getClass().getMethod(getterName, parameter).invoke(instanze);
+                if (embedded == null) {
+                  Constructor<?> cons = parameters[0].getConstructor(parameter);
+                  embedded = cons.newInstance();
+                  meth.invoke(instanze, embedded);
+                }
+                if (embedded != null) {
+                  setAttributesDeep((Map<String, Object>) value, embedded);
+                }
+              }
+            } catch (IllegalAccessException e) {
+              throw new ODataJPAProcessorException(e, HttpStatusCode.INTERNAL_SERVER_ERROR);
+            } catch (IllegalArgumentException e) {
+              throw new ODataJPAProcessorException(e, HttpStatusCode.INTERNAL_SERVER_ERROR);
+            } catch (InvocationTargetException e) {
+              throw new ODataJPAProcessorException(e, HttpStatusCode.INTERNAL_SERVER_ERROR);
+            } catch (NoSuchMethodException e) {
+              throw new ODataJPAProcessorException(e, HttpStatusCode.INTERNAL_SERVER_ERROR);
+            } catch (SecurityException e) {
+              throw new ODataJPAProcessorException(e, HttpStatusCode.INTERNAL_SERVER_ERROR);
+            } catch (InstantiationException e) {
               throw new ODataJPAProcessorException(e, HttpStatusCode.INTERNAL_SERVER_ERROR);
             }
           }
