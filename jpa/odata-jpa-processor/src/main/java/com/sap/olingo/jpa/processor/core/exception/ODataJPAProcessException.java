42ebcd62ac5429da3833998c86e79c52458c5539
Oliver Grande <oliver.grande@sap.com>
2017-04-04 17:48:36 +0200

    Allow overwrite getTextBundle()

--- jpa/odata-jpa-processor/src/main/java/com/sap/olingo/jpa/processor/core/exception/ODataJPAProcessException.java
+++ jpa/odata-jpa-processor/src/main/java/com/sap/olingo/jpa/processor/core/exception/ODataJPAProcessException.java
@@ -14,13 +14,13 @@ public abstract class ODataJPAProcessException extends ODataApplicationException
   /**
    * 
    */
-  private static final long serialVersionUID = -3178033271311091314L;
-  private static final String UNKNOWN_MESSAGE = "No message text found";
+  private static final long          serialVersionUID = -3178033271311091314L;
+  private static final String        UNKNOWN_MESSAGE  = "No message text found";
   private static Enumeration<Locale> locales;
 
-  protected final String id;
+  protected final String   id;
   protected final String[] parameter;
-  protected final String messageText;
+  protected final String   messageText;
 
   public ODataJPAProcessException(final String id, final HttpStatusCode statusCode) {
     this(id, null, statusCode, new String[] {});
@@ -71,7 +71,7 @@ public abstract class ODataJPAProcessException extends ODataApplicationException
     this.messageText = messageText;
   }
 
-  private ODataJPAMessageTextBuffer getTextBundle() {
+  protected ODataJPAMessageTextBuffer getTextBundle() {
     if (getBundleName() != null)
       return new ODataJPAMessageTextBuffer(getBundleName(), locales);
     else
