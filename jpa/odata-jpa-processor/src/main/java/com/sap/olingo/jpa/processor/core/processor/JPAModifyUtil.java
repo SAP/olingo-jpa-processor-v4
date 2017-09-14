08c6d3e041e462c95fb38162aebae3cf5ba1f1ad
Oliver Grande <oliver.grande@sap.com>
2017-09-14 10:33:42 +0200

    Add method to fill the key to JPAModifyUtil

--- jpa/odata-jpa-processor/src/main/java/com/sap/olingo/jpa/processor/core/processor/JPAModifyUtil.java
+++ jpa/odata-jpa-processor/src/main/java/com/sap/olingo/jpa/processor/core/processor/JPAModifyUtil.java
@@ -3,14 +3,17 @@ package com.sap.olingo.jpa.processor.core.processor;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.math.BigInteger;
 import java.util.Map;
 
 import org.apache.olingo.commons.api.http.HttpStatusCode;
 
+import com.sap.olingo.jpa.metadata.core.edm.mapper.api.JPAEntityType;
+import com.sap.olingo.jpa.metadata.core.edm.mapper.exception.ODataJPAModelException;
 import com.sap.olingo.jpa.processor.core.exception.ODataJPAProcessorException;
 
 /**
- * This class provides some primitive util methods to support modifying operations like create, update or clean.<p>
+ * This class provides some primitive util methods to support modifying operations like create or update.<p>
  * The set method shall fill an object from a given Map. JPA processor provides in a Map the internal, JAVA attribute,
  * names. Based on the JAVA naming conventions the corresponding Setter is called, as long as the Setter has the correct
  * type.
@@ -18,6 +21,33 @@ import com.sap.olingo.jpa.processor.core.exception.ODataJPAProcessorException;
  *
  */
 public final class JPAModifyUtil {
+  /**
+   * Create a filled instance of a JPA entity key.<p>
+   * For JPA entities having only one key, so do not use an IdClass only the types defined by <code>isPrimitive</code>
+   * are supported.
+   * @param et
+   * @param jpaKeys
+   * @return
+   * @throws ODataJPAProcessorException
+   */
+  public Object createPrimaryKey(final JPAEntityType et, final Map<String, Object> jpaKeys)
+      throws ODataJPAProcessorException {
+    try {
+      Object key = null;
+      if (isPrimitive(et.getKeyType())) {
+        Object keyValue = jpaKeys.get(et.getKey().get(0).getInternalName());
+        key = et.getKeyType().getConstructor(String.class).newInstance(keyValue.toString());
+      } else {
+        key = et.getKeyType().getConstructor().newInstance();
+        setAttributes(jpaKeys, key);
+      }
+      return key;
+    } catch (NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException
+        | IllegalArgumentException | InvocationTargetException | ODataJPAModelException e) {
+      throw new ODataJPAProcessorException(e, HttpStatusCode.INTERNAL_SERVER_ERROR);
+    }
+  }
+
   /**
    * 
    * @param jpaAttributes
@@ -38,11 +68,7 @@ public final class JPAModifyUtil {
               if (parameters.length == 1 && (value == null || value.getClass() == parameters[0])) {
                 meth.invoke(instanze, value);
               }
-            } catch (IllegalAccessException e) {
-              throw new ODataJPAProcessorException(e, HttpStatusCode.INTERNAL_SERVER_ERROR);
-            } catch (IllegalArgumentException e) {
-              throw new ODataJPAProcessorException(e, HttpStatusCode.INTERNAL_SERVER_ERROR);
-            } catch (InvocationTargetException e) {
+            } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
               throw new ODataJPAProcessorException(e, HttpStatusCode.INTERNAL_SERVER_ERROR);
             }
           }
@@ -107,5 +133,18 @@ public final class JPAModifyUtil {
         }
       }
     }
+  }
+
+  public boolean isPrimitive(Class<?> keyType) {
+    return String.class.equals(keyType) ||
+        Integer.class.equals(keyType) ||
+        Long.class.equals(keyType) ||
+        Short.class.equals(keyType) ||
+        BigInteger.class.equals(keyType) ||
+        Character.class.equals(keyType) ||
+        Byte.class.equals(keyType) ||
+        Float.class.equals(keyType) ||
+        Boolean.class.equals(keyType) ||
+        Double.class.equals(keyType);
   }
 }
