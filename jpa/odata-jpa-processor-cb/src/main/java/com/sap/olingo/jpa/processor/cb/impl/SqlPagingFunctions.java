91ebd6ed473781cf73474b926c1313635a10fff1
Oliver Grande <oliver.grande@sap.com>
2024-03-26 09:02:39 +0100

    Defect/eclipse link hana problem (#325)
    * Handle LIMIT and OFFSET with TypedQuery
    
    * Update default for LIMIT

--- jpa/odata-jpa-processor-cb/src/main/java/com/sap/olingo/jpa/processor/cb/impl/SqlPagingFunctions.java
+++ jpa/odata-jpa-processor-cb/src/main/java/com/sap/olingo/jpa/processor/cb/impl/SqlPagingFunctions.java
@@ -1,7 +1,7 @@
 package com.sap.olingo.jpa.processor.cb.impl;
 
 enum SqlPagingFunctions {
-  LIMIT("LIMIT", -1),
+  LIMIT("LIMIT", Integer.MAX_VALUE),
   OFFSET("OFFSET", 0);
 
   private final String keyWord;
