9054f6f2f6a2336a09efc02f42bc923c53a8fcd3
Oliver Grande <oliver.grande@sap.com>
2024-10-17 10:17:42 +0200

    Update pom (#350)

--- jpa/odata-jpa-metadata/src/main/java/com/sap/olingo/jpa/metadata/core/edm/mapper/extension/ODataAction.java
+++ jpa/odata-jpa-metadata/src/main/java/com/sap/olingo/jpa/metadata/core/edm/mapper/extension/ODataAction.java
@@ -2,8 +2,8 @@ package com.sap.olingo.jpa.metadata.core.edm.mapper.extension;
 
 /**
  * Tag Interface to indicate that a java class represents an OData <a href =
- * "http://docs.oasis-open.org/odata/odata/v4.0/errata02/os/complete/part3-csdl/odata-v4.0-errata02-os-part3-csdl-complete.html#_Toc406398010">
- * Function.</a>
+ * "https://docs.oasis-open.org/odata/odata/v4.0/errata02/os/complete/part3-csdl/odata-v4.0-errata02-os-part3-csdl-complete.html#_Toc406398005">
+ * Action.</a>
  * <p>
  * An implementing class may provide <b>one</b> constructor having no parameter or a combination of the following:
  * <ul>
